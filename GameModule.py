bcde8373e0289081d6fd81799c5785883014178f
Lorin Budaca <lorinbudaca@gmail.com>
2022-01-06 18:20:28 +0200

    Implemented state properties.

--- GameModule.py
+++ GameModule.py
@@ -4,6 +4,49 @@ class Table:
     def __init__(self, pixels):
         self.pixels = pixels
 
+    def getMaxHeights(self):
+        numberOfLines = len(self.pixels)
+        maxHeights = [0 for _ in range(len(self.pixels[0]))]
+        for lineIndex in range(len(self.pixels)):
+            line = self.pixels[lineIndex]
+
+            for columnIndex in range(len(line)):
+                if maxHeights[columnIndex] == 0 and line[columnIndex] != 0:
+                    maxHeights[columnIndex] = numberOfLines - lineIndex
+
+        return maxHeights
+
+    def getNumberOfHoles(self):
+        maxHeights = self.getMaxHeights()
+        holes = 0
+        numberOfLines = len(self.pixels)
+
+        for columnIndex in range(len(self.pixels[0])):
+            for rowIndex in range(numberOfLines - maxHeights[columnIndex], numberOfLines):
+                if self.pixels[rowIndex][columnIndex] == 0:
+                    holes += 1
+
+        return holes
+
+    def fullLines(self):
+        return sum(
+            map(
+                lambda line: all(map(lambda pixel: pixel != 0, line)),
+                self.pixels
+            )
+        )
+
+    def bumpiness(self):
+        bumpiness = 0
+        maxHeights = self.getMaxHeights()
+        for columnIndex in range(len(maxHeights) - 1):
+            bumpiness += abs(maxHeights[columnIndex] - maxHeights[columnIndex + 1])
+
+        return bumpiness
+
+    def totalHeight(self):
+        return sum(self.getMaxHeights())
+
 
 class State:
 
@@ -11,3 +54,25 @@ class State:
 
     def __init__(self, table: Table):
         self.table = table
+
+    def getStateProperties(self):
+        return BoardProperties(
+            holes=self.table.getNumberOfHoles(),
+            fullLines=self.table.fullLines(),
+            bumpiness=self.table.bumpiness(),
+            totalHeight=self.table.totalHeight()
+        )
+
+
+class BoardProperties:
+
+    holes: int
+    fullLines: int
+    bumpiness: int
+    totalHeight: int
+
+    def __init__(self, holes: int, fullLines: int, bumpiness: int, totalHeight: int):
+        self.holes = holes
+        self.fullLines = fullLines
+        self.bumpiness = bumpiness
+        self.totalHeight = totalHeight
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import GameModule as gm
 
 if __name__ == '__main__':
     displayManager = dm.DisplayManager()
-    displayManager.displayState(gm.State(
+    state = gm.State(
         gm.Table([
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -20,13 +20,17 @@ if __name__ == '__main__':
             [9, 9, 0, 0, 0, 0, 0, 0, 0, 0],
             [8, 9, 0, 0, 0, 0, 0, 0, 0, 0],
             [8, 8, 8, 5, 4, 0, 0, 2, 1, 0],
-            [6, 5, 5, 5, 4, 4, 2, 2, 1, 0],
+            [6, 5, 0, 5, 4, 4, 2, 2, 1, 0],
             [6, 7, 7, 1, 4, 3, 2, 2, 1, 10]
         ])
-    ))
+    )
+    displayManager.displayState(state)
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+
+
